5bfd77bcd1e9d7eb1b352f65b11e4e0caa5ba06d
Cristian Petra <cpetra@qlaunch.de>
2026-06-02 15:00:57 +0300

    Initial Commit

---
+++ INGCampaign/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  INGCampaign
+//
+//  Created by Cristian Petra on 02.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ INGCampaign/INGCampaignApp.swift
@@ -0,0 +1,17 @@
+//
+//  INGCampaignApp.swift
+//  INGCampaign
+//
+//  Created by Cristian Petra on 02.06.26.
+//
+
+import SwiftUI
+
+@main
+struct INGCampaignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
