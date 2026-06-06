30d3b4afb42a5b02a384182cba28f8eda1e44cf6
Cristian Petra <cpetra@qlaunch.de>
2026-06-06 18:07:51 +0300

    Add AppRoute and router

---
+++ INGCampaign/src/Navigation/AppRoute.swift
@@ -0,0 +1,86 @@
+//
+//  AppRoute.swift
+//  INGCampaign
+//
+//  Created by Cristian Petra on 04.06.26.
+//
+import SwiftUI
+import INGCampaignModels
+internal import Combine
+
+
+protocol AppRouterProtocol {
+    func navigate(to route: CampaignRoute)
+}
+
+enum AppRoute: Hashable {
+    case campaign(CampaignRoute)
+}
+
+enum CampaignRoute: Hashable {
+    case targetSelection
+    case channelList(channels: [CampaignChannel])
+    //case channelList
+    case campaignSelection(channel: CampaignChannel)
+}
+
+protocol ViewFactory {
+    associatedtype V: View
+    @ViewBuilder func view(container: AppDependencyContainer) -> V
+}
+
+
+
+@MainActor
+class AppRouter: ObservableObject {
+    @Published var path: NavigationPath = NavigationPath()
+    
+    func navigate(to route: CampaignRoute) {
+        path.append(route)
+    }
+    
+    func goBack() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+
+    func goToRoot() {
+        path.removeLast(path.count)
+    }
+    
+}
+
+extension AppRouter: AppRouterProtocol {}
+
+
+//@MainActor
+//public final class CampaignRouter: ObservableObject {
+//    @Published public var path: [CampaignRoute] = []
+//    public init() {}
+//    public func navigate(to route: CampaignRoute) { path.append(route) }
+//    public func goBack()   { if !path.isEmpty { path.removeLast() } }
+//    public func goToRoot() { path.removeAll() }
+//}
+
+@MainActor
+class CampaignRouter: ObservableObject, AppRouterProtocol {
+    @Published public var path: [CampaignRoute] = []
+    
+    func navigate(to route: CampaignRoute) {
+        path.append(route)
+    }
+    
+    func goBack() {
+        guard !path.isEmpty else { return }
+        path.removeLast()
+    }
+
+    func goToRoot() {
+        path.removeLast(path.count)
+    }
+    
+}
+
+
+
+
---
+++ INGCampaign/src/Navigation/CampaignFlowView.swift
@@ -0,0 +1,51 @@
+//
+//  CampaignFlowView.swift
+//  INGCampaign
+//
+//  Created by Cristian Petra on 04.06.26.
+//
+
+import SwiftUI
+
+struct CampaignFlowView: View {
+    
+    @StateObject private var router = CampaignRouter()
+    let container: DependencyContainer
+
+    var body: some View {
+        NavigationStack(path: $router.path) {
+            destination(.targetSelection)
+                .navigationDestination(for: CampaignRoute.self, destination: destination)
+        }
+    }
+
+    @ViewBuilder
+    private func destination(_ route: CampaignRoute) -> some View {
+        switch route {
+        case .targetSelection:
+            TargetingSelectionView(
+                viewModel: container.makeTargetingSelectionViewModel(
+                    router: router
+                )
+            )
+        case .channelList(let channels):
+            ChannelListView(
+                viewModel: container.makeChannelListViewModel(
+                    campaignChannels:channels,
+                    router: router
+                )
+            )
+        case .campaignSelection(let channel):
+            CampaignSelectionView(
+                viewModel: container.makeCampaingSelectionViewModel(
+                    campaignChannel: channel,
+                    router: router
+                )
+            )
+        }
+    }
+}
+
+#Preview {
+    //CampaignFlowView()
+}
